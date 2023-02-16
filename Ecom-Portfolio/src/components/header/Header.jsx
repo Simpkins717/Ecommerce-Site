6652dbad5f2f2e159af6e56cd4c7ee85c12ed0c7
Simpkins717 <dsimpkins717@gmail.com>
2023-02-16 07:34:16 -0500

    moved header.jsx

--- Ecom-Portfolio/src/components/header/Header.jsx
+++ Ecom-Portfolio/src/components/header/Header.jsx
@@ -3,9 +3,22 @@ import './header.css';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 export const Header = () => {
+  window.addEventListener('scroll', function () {
+    const header = this.document.querySelector('.header');
+    header.classList.toggle('active', this.window.scrollY > 100);
+  });
+  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
   return (
     <>
-      <h1>Header</h1>
+      <header className='header'>
+        <div className='scontainer flex'>
+          <div className='logo'></div>
+          <div className='search flex'>
+            <AiOutlineSearch className='searchIcon' />
+            <input type='text' placeholder='Search...' />
+          </div>
+        </div>
+      </header>
     </>
   );
 };
--- Ecom-Portfolio/src/pages/home/Home.jsx
+++ Ecom-Portfolio/src/pages/home/Home.jsx
@@ -1,25 +1,7 @@
 import React from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
 
 function Home() {
-  window.addEventListener('scroll', function () {
-    const header = this.document.querySelector('.header');
-    header.classList.toggle('active', this.window.scrollY > 100);
-  });
-  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-  return (
-    <>
-      <header className='header'>
-        <div className='scontainer flex'>
-          <div className='logo'></div>
-          <div className='search flex'>
-            <AiOutlineSearch className='searchIcon' />
-            <input type='text' placeholder='Search...' />
-          </div>
-        </div>
-      </header>
-    </>
-  );
+  return <>Home</>;
 }
 
 export { Home };
